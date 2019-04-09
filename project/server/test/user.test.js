9c2d366b30d97386681505f1985302dd1bab225a
chipcary <chipcary@gmail.com>
2019-04-08 20:01:14 -0400

    set up unit testing and made db directory

---
+++ project/server/test/user.test.js
@@ -0,0 +1,37 @@
+const assert = require('assert');
+const expect = require('chai').expect
+const request = require('supertest');
+const app = require('../app')
+
+//load the handmade test db
+//call is synchronous bc unit testing requires the db to be reset
+const execSync = require('child_process').execSync;
+code = execSync('../db/smalldb/resetdb.sh');
+
+describe('Unit testing the /users route', function() {
+
+    it('should return get user info', function() {
+      return request(app)
+        .get('/api/users/bhc17@duke.edu')
+        .then(function(response){
+            assert.equal(response.status, 200);
+            data = response.body[0];
+            assert.equal(data["name"], "Chip");
+        });
+    });
+
+    it('should delete a user', function() {
+        return request(app)
+        .delete('/api/users/bhc17@duke.edu')
+        .then(function(response){
+            return request(app)
+            	.get('/api/users/bhc17@duke.edu')
+        		.then(function(res2){
+        		console.log(res2);
+            	assert.equal(res2.status, 200);
+            	assert.equal(res2.body.length, 0);            	
+        	});
+        });
+    });
+
+});
