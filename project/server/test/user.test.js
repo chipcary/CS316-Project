fa3c75e2d36b6216ec6d253e4fde4ff6e6d7970c
chipcary <chipcary@gmail.com>
2019-04-08 20:32:11 -0400

    testing refactor

--- project/server/test/user.test.js
+++ project/server/test/user.test.js
@@ -1,15 +1,15 @@
+//load the handmade test db
+//call is synchronous bc unit testing requires the db to be reset
 const assert = require('assert');
 const expect = require('chai').expect
 const request = require('supertest');
 const app = require('../app')
 
-//load the handmade test db
-//call is synchronous bc unit testing requires the db to be reset
-const execSync = require('child_process').execSync;
-code = execSync('../db/smalldb/resetdb.sh');
+
+
 
 describe('Unit testing the /users route', function() {
-
+	
     it('should return get user info', function() {
       return request(app)
         .get('/api/users/bhc17@duke.edu')
@@ -27,7 +27,6 @@ describe('Unit testing the /users route', function() {
             return request(app)
             	.get('/api/users/bhc17@duke.edu')
         		.then(function(res2){
-        		console.log(res2);
             	assert.equal(res2.status, 200);
             	assert.equal(res2.body.length, 0);            	
         	});
